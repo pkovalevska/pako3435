1f6203f7b1164ee29a211a83211cb3ce85a39335
pkovalevska <pkovalevska@gmail.com>
2025-09-22 00:45:33 +0200

    Updating About page
    Updating About page

--- pages/About.py
+++ pages/About.py
@@ -5,7 +5,7 @@ st.title("Patricija Kovalevska")
 st.markdown("""
 ## About My Project in the DSHI Course
 
-During the DSHI course, I worked on a project using the **UCI Heart Disease dataset** to predict whether a patient has heart disease or not. I cleaned and prepared the data, handled missing values, and selected key features like **chest pain type, blood pressure, and cholesterol**. I explored the data with charts and statistics to understand patterns between variables.
+During the DSHI course, I worked on a project using the **UCI Heart Disease dataset** to predict whether a patient has heart disease or not. I cleaned and prepared the data, handled missing values, and selected key features like **chest pain type, blood pressure, and cholesterol**. I explored the data to identify patterns between variables using bivariate analytical questions and data visualization.
 
 For modeling, I transformed the target into a **binary variable** and trained several classifiers, including:
 - Decision Trees  
